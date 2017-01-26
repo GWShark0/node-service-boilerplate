57f2dd4c3a045f0875986733d41ee2b0ebb5865b
Greg Douglas <gwshark0@gmail.com>
2017-01-26 16:31:59 -0500

    Rudimentary express server

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 require('dotenv').config();
 
+const express = require('express');
+const app = express();
+
+const index = require('./routes/index');
+
+const { PORT } = process.env;
+
+app.use(index);
+
+app.listen(PORT, () => {
+  console.log('Example app listening on port 3000!');
+});
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.send('Hello World');
+});
+
+router.get('/status', (req, res, next) => {
+  res.json({status: 'OK'});
+});
+
+module.exports = router;
